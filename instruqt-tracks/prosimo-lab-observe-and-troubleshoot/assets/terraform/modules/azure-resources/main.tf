e4302123e2ba43dc138ca90c1e17c51db2c4809d
iracic <iracic@debian-terra.iracictechguru.com>
2023-10-19 10:21:13 -0400

    add SG

--- instruqt-tracks/prosimo-lab-observe-and-troubleshoot/assets/terraform/modules/azure-resources/main.tf
+++ instruqt-tracks/prosimo-lab-observe-and-troubleshoot/assets/terraform/modules/azure-resources/main.tf
@@ -138,19 +138,23 @@ resource "azurerm_network_security_rule" "allow_tcp_5000" {
 
 # Allow SSH traffic
 resource "azurerm_network_security_rule" "allow_ssh" {
-  name                        = "allow-ssh"
-  priority                    = 1003  # Adjust the priority as needed
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "22"
-  source_address_prefixes     = [
+  name                    = "allow-ssh"
+  priority                = 1003  # Adjust the priority as needed
+  direction               = "Inbound"
+  access                  = "Allow"
+  protocol                = "Tcp"
+  source_port_range       = "*"
+  destination_port_range  = "22"
+  source_address_prefixes = [
     "0.0.0.0/0",
   ]
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg_iac.name
+  network_security_group_name = azurerm_network_security_group.example.name
+}
 
   # Allow UDP port 5201 traffic
-resource "azurerm_network_security_rule" "allow_tcp_5000" {
+resource "azurerm_network_security_rule" "allow_udp_5201" {
   name                        = "allow-udp-5201"
   priority                    = 1004  # Adjust the priority as needed
   direction                   = "Inbound"
@@ -188,6 +192,9 @@ resource "azurerm_network_security_rule" "allow_tcp_5201" {
     "10.5.0.0/24",
     "10.6.0.0/24",
   ]
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg_iac.name
+  network_security_group_name = azurerm_network_security_group.example.name
 }
 
 # Associate the NSG with the network interface
