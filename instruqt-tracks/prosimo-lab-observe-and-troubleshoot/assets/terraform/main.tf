cc5bd23fa73208b0e15749394b62252c184ae806
iracic <iracic@debian-terra.iracictechguru.com>
2023-12-06 06:04:08 -0500

    add scripts

--- instruqt-tracks/prosimo-lab-observe-and-troubleshoot/assets/terraform/main.tf
+++ instruqt-tracks/prosimo-lab-observe-and-troubleshoot/assets/terraform/main.tf
@@ -91,8 +91,40 @@ resource "prosimo_cloud_creds" "azure" {
   }
 }
 
-# Create Prosimo Infra resources
+# Create Prosimo Infra resources in AWS
 
+module "prosimo_resource_aws_eu" {
+  source     = "./modules/prosimo-resources"
+  prosimo_teamName = var.prosimo_teamName
+  prosimo_token = var.prosimo_token
+  prosimo_cidr       = var.prosimo_cidr[0]
+  cloud = "AWS"
+  cloud1 = "Prosimo_AWS"
+  apply_node_size_settings = "true"
+  bandwidth = "<1 Gbps"
+  instance_type = "t3.medium"
+  multipleRegion = var.aws_region[0]
+  wait = "false"
+
+}
+
+
+module "prosimo_resource_aws_us" {
+  source     = "./modules/prosimo-resources"
+  prosimo_teamName = var.prosimo_teamName
+  prosimo_token = var.prosimo_token
+  prosimo_cidr       = var.prosimo_cidr[1]
+  cloud = "AWS"
+  cloud1 = "Prosimo_AWS"
+  apply_node_size_settings = "true"
+  bandwidth = "<1 Gbps"
+  instance_type = "t3.medium"
+  multipleRegion = var.aws_region[1]
+  wait = "false"
+
+}
+
+/*
 module "prosimo_resource_aws" {
   source     = "./modules/prosimo-resources"
   prosimo_teamName = var.prosimo_teamName
@@ -108,6 +140,7 @@ module "prosimo_resource_aws" {
   wait = "false"
   
 }
+*/
 
 module "prosimo_resource_azure" {
   source     = "./modules/prosimo-resources"
