84200924b15855371ebb3db577deb947bf36c6ec
iracic <iracic@debian-terra.iracictechguru.com>
2024-02-29 14:39:16 -0500

    adding TF verison in providers

--- instruqt-tracks/prosimo-lab-observe-and-troubleshoot/assets/terraform/main.tf
+++ instruqt-tracks/prosimo-lab-observe-and-troubleshoot/assets/terraform/main.tf
@@ -148,7 +148,7 @@ module "prosimo_resource_azure" {
   prosimo_token = var.prosimo_token
   prosimo_cidr       = "10.253.0.0/23"
   cloud = "AZURE"
-  apply_node_size_settings = "false"
+  apply_node_size_settings = "true"
   cloud1 = "Prosimo_Azure"
   multipleRegion = "northeurope"
   wait = "false"
